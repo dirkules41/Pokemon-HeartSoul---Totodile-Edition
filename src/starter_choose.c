adbf09820d1c07aa4d59379344c133c58a6fa2ba
dirkules41 <max.liebler.psn@gmail.com>
2026-08-25 20:22:09 +0200

    Update starter_choose.c

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -754,10 +754,10 @@ static u8 CreatePokemonFrontSpriteChikorita(u16 species, u8 x, u8 y)
 static u8 CreatePokemonFrontSpriteTotodile(u16 species, u8 x, u8 y)
 {
     u8 spriteId;
-    static u32 isShinyTotodile = 0;
+    static u32 isShinyTotodile = 1;
 
     if (isShinyTotodile == 0)
-        isShinyTotodile = (Random()% 128);
+        isShinyTotodile = (Random()% 8192);
 
     if ((isShinyTotodile < SHINY_ODDS) && (gSaveBlock1Ptr->tx_Features_ShinyChance == 0)) // 1/8192
     {
